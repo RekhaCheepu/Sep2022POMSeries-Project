cee9cf7bf5469efd6c939dbeeda6e0c42773a195
Rekharani <122629816+RekhaCheepu@users.noreply.github.com>
2023-01-17 14:19:16 -0500

    Create POC.java

---
+++ src/main/java/com/qa/opencart/pages/POC.java
@@ -0,0 +1,21 @@
+package com.qa.opencart.pages;
+
+import org.openqa.selenium.By;
+
+public class POC {
+
+	public static void main(String[] args) {
+	int a=10;
+	int b= 5;
+	System.out.println(a+b);
+	}
+
+	By cart =By.id("POC");
+	public POC() {
+		
+	}
+	public void POCmethod() {
+		System.out.println("POC is done");
+	}
+
+}
