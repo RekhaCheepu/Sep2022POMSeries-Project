14bf697038ec80948a9413665bdf314fdc25ab18
RekhaCheepu <rekha.cheepu@gmail.com>
2023-01-17 23:03:36 -0500

    new ordermethod added

--- src/main/java/com/qa/opencart/pages/OrderPage.java
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -4,7 +4,8 @@ public class OrderPage {
 
 	public static void main(String[] args) {
 		int id=12345678;
-
-	}
-
+		}
+public void orderpageMethod() {
+	System.out.println("order page method is compleated");
+}
 }
